b1bf58fab2503eae94273165fa07388755c5388b
unknown <abdoelsharef1@gmail.com>
2022-11-05 15:33:50 +0200

    new modfying

--- intermediat_python/Screenshot_taker.py
+++ intermediat_python/Screenshot_taker.py
@@ -1,4 +1,11 @@
 import pyautogui
 
-screen = pyautogui.screenshot()
-screen.save('J:\Abdo_BackEnd\Django_FullStack_Project\\Python_Dev\\01_Python_Basics\\new_screen.png')
+
+def screen_shot():
+    screen = pyautogui.screenshot()
+    screen.save(
+        'J:\Abdo_BackEnd\Django_FullStack_Project\\Python_Dev\\01_Python_Basics\\new_screen.png')
+    print("screen shot is done".title())
+
+
+screen_shot()
